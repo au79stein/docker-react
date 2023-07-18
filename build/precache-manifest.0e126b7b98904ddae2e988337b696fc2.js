9ee7efad56a2dd09e4f457ac29b19b8ba21f24fb
Rich <rich@datarich.org>
2023-07-17 23:05:36 -0400

    initial commit 2nd attempt

---
+++ build/precache-manifest.0e126b7b98904ddae2e988337b696fc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec687ad0e1005c1a8a5f",
+    "url": "/static/js/main.26859ed3.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ec687ad0e1005c1a8a5f",
+    "url": "/static/css/main.ff3a1b0a.chunk.css"
+  },
+  {
+    "revision": "808fdb81bf670af1dc04f70e3b126942",
+    "url": "/index.html"
+  }
+];
